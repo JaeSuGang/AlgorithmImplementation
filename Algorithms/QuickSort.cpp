11abfd965bc39f91452505acc77224359f9641c2
Arbitrum <33kkm33@gmail.com>
2025-03-28 00:35:30 +0900

    퀵정렬 구현 미완성

--- Algorithms/QuickSort.cpp
+++ Algorithms/QuickSort.cpp
@@ -1,1 +1,52 @@
 #include "QuickSort.h"
+
+void quicksort(int* pArray, int nSize)
+{
+	int nTemp;
+
+	switch (nSize)
+	{
+	case 0:
+	case 1:
+		break;
+
+	case 2:
+	{
+		if (pArray[0] > pArray[1])
+		{
+			nTemp = pArray[0];
+			pArray[0] = pArray[1];
+			pArray[1] = nTemp;
+		}
+		break;
+	}
+
+	default:
+	{
+		constexpr int nPivotIndex = 0;
+		int nLeftIndex = 0;
+		int nRightIndex = nSize - 1;
+
+		while (nLeftIndex <= nRightIndex)
+		{
+			while (pArray[nLeftIndex] <= pArray[nPivotIndex])
+				nLeftIndex++;
+
+			while (pArray[nRightIndex] >= pArray[nPivotIndex])
+				nRightIndex--;
+
+			nTemp = pArray[nLeftIndex];
+			pArray[nLeftIndex] = pArray[nRightIndex];
+			pArray[nRightIndex] = nTemp;
+
+		}
+
+		quicksort(pArray, nLeftIndex);
+		quicksort(pArray + nLeftIndex, nSize - nLeftIndex);
+
+		break;
+	}
+	}
+
+	return;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-﻿
+﻿#include <iostream>
+
+#include "Algorithms/QuickSort.h"
 
 
 int main()
 {
+	int a[10] = { 4, 2, 3, 9, 5, 1, 7, 6, 8, 0 };
 
+	quicksort(a, 10);
+
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << a[i] << std::endl;
+	}
 }
 
